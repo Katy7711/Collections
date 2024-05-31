b040d34ba7623f8cf92da54cfe3a074030d595f6
katy7711 <deniev777@yandex.ru>
2024-05-31 13:29:37 +0500

    HashMap

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -9,6 +12,10 @@ public class Main {
     ArrayList<String> arrayList = new ArrayList<>(Arrays.asList("one", "two", "three"));
     Applying applying = new Applying();
     System.out.println(filter(arrayList, applying));
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,2,3,3,3,4,4,4,4));
+    System.out.println(elements(numbers));
+
+
 
   }
   public static ArrayList<String> filter(ArrayList<String> objects, Applying applying){
@@ -17,5 +24,14 @@ public class Main {
       str.add(applying.apply(i).toString());
     }
     return str;
+   }
+
+   public static Map<Integer, Integer> elements (ArrayList<Integer> objects){
+     Map<Integer, Integer> elements = new HashMap<>();
+     for (Integer i : objects) {
+       int count = Collections.frequency(objects, i);
+       elements.put(i, count);
+     }
+     return elements;
    }
 }
